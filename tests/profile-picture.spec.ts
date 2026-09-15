02508e4242aba3c9a03a6bd874430251e5818d18
LincolnX2 <sabur.yinus@gmail.com>
2026-09-15 23:07:37 +0200

    create profile picture file

---
+++ tests/profile-picture.spec.ts
@@ -0,0 +1,29 @@
+import { test, expect } from '@playwright/test';
+import { LoginPage } from '../src/pages/LoginPage';
+import { ProfilePage } from '../src/pages/ProfilePage';
+import { ApiValidator } from '../src/utils/api-validator';
+import { ReportGenerator, TestReport, TestStep } from '../src/utils/report-generator';
+import { TestHelpers } from '../src/utils/test-helpers';
+import { environment } from '../config/environment.config';
+import path from 'path';
+import fs from 'fs';
+
+const TEST_EMAIL = environment.testEmail;
+const TEST_PASSWORD = environment.testPassword;
+
+test.describe('Ndosi Profile Picture Update Flow', () => {
+  let loginPage: LoginPage;
+  let profilePage: ProfilePage;
+  let apiValidator: ApiValidator;
+  let reportGenerator: ReportGenerator;
+  let testSteps: TestStep[] = [];
+  let startTime: number;
+
+  test.beforeEach(async ({ page }) => {
+    loginPage = new LoginPage(page);
+    profilePage = new ProfilePage(page);
+    apiValidator = new ApiValidator();
+    reportGenerator = new ReportGenerator();
+    testSteps = [];
+    startTime = Date.now();
+  });
